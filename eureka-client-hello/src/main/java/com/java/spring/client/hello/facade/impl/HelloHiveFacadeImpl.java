5a3f230fc25b7ec19648733f394514833c57dcba
renqingwang <renqingwang@wanda.cn>
2017-09-02 08:29:05 +0800

    【hive】代码结构优化

--- eureka-client-hello/src/main/java/com/java/spring/client/hello/facade/impl/HelloHiveFacadeImpl.java
+++ eureka-client-hello/src/main/java/com/java/spring/client/hello/facade/impl/HelloHiveFacadeImpl.java
@@ -1,6 +1,7 @@
 package com.java.spring.client.hello.facade.impl;
 
-import com.java.spring.client.hello.facade.HelloWorldFacade;
+import com.java.spring.client.hello.common.condition.ConditionServiceConfirm;
+import com.java.spring.client.hello.facade.HelloHiveFacade;
 import com.java.spring.client.hello.facade.vo.HelloHiveVO;
 import com.java.spring.client.hello.service.HiveExecuteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,12 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 /**
  * @author renqingwang on 2017/8/14.
  * @version 1.0
  */
 @RestController
-public class HelloWorldFacadeImpl implements HelloWorldFacade{
+public class HelloHiveFacadeImpl implements HelloHiveFacade {
    @Autowired
    private HiveExecuteService hiveExecuteService;
 
